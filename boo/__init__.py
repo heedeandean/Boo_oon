8a5c744c87ff56de6cbb11e5684453f9c8f05f0f
eeuoo <lee512joo@gmail.com>
2019-03-12 13:13:32 +0900

    first commit

---
+++ boo/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask, url_for, render_template, request, Response, session, jsonify, make_response, redirect, flash
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/boo')
+def main():
+    return render_template('main.html')
