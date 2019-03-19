0e49094e0d903d0eee9f0bd9da71b27b81d82aec
eeuoo <lee512joo@gmail.com>
2019-03-19 21:28:30 +0900

    lee

--- boo/__init__.py
+++ boo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, request, Response, session, jsonify, make_response, redirect, flash
+from flask import Flask, url_for, render_template, request, Response, session, jsonify, make_response, redirect, flash, json
 
 app = Flask(__name__)
 app.debug = True
@@ -6,3 +6,7 @@ app.debug = True
 @app.route('/boo')
 def main():
     return render_template('ecom_main.html')
+
+
+
+  
