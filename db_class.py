9fe0813ec8579b18f072688e620ae5323a460635
eeuoo <lee512joo@gmail.com>
2019-03-19 16:06:19 +0900

    class 완성

--- db_class.py
+++ db_class.py
@@ -1,76 +1,81 @@
-from sqlalchemy import Column, Integer, Float, String, ForeignKey, PrimaryKeyConstraint, func
+from sqlalchemy import Column, Integer, Float, String, Boolean, ForeignKey, PrimaryKeyConstraint, func
 from sqlalchemy.orm import relationship, backref
 from init_db import Base
 
 class User(Base):
     _tablename_ = 'User'
-    userno = Column (int unsigned not null auto_increment primary key,
-    username varchar(128) not null,
-    pw varchar(256) not null,
-    birthdate varchar(13)  NOT NULL, 
-    city varchar(128) not null,
-    gender varchar(5) not null,
-    join_date  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    job varchar(128) not null default '',
-    email varchar(128) unique,
-    following_cnt int(11) not null default 0,
-    follower_cnt int(11) not null default 0,
-    flag boolean,
+    userno = Column(Integer, primary_key=True)
+    username  = Column(String)
+    pw = Column(String)
+    birthdate = Column(String)
+    city = Column(String)
+    gender = Column(String)
+    join_date  =  Column(String)
+    job = Column(String)
+    email = Column(String)
+    following_cnt =  Column(Integer)
+    follower_cnt = Column(Integer)
+    flag = Column(Boolean)
 
 
-create table Follow (					
-	userno int unsigned not null,
-    	following int unsigned not null,
+class Follow(Base):	
+    __tablename__ = 'Follow'			
+    userno = Column(Integer)
+    following = Column(Integer)
+
+    fk_user = relationship('User')
     
-    	constraint foreign key fk_user(userno) references User(userno)
-);
 
-create table List (
-	list_id int unsigned not null auto_increment primary key,
-    	userno int unsigned not null,
-    	list_txt varchar(4096),
-    	likecnt int(11) not null default 0,
-    	hatecnt int(11) not null default 0,
-    	public boolean,
-    	list_date timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+class List(Base):
+    _tablename_ = "List"
+    list_id = Column(Integer, primary_key=True)
+    userno = Column(Integer)
+    list_txt = Column(String)
+    likecnt = Column(Integer)
+    hatecnt = Column(Integer)
+    public = Column(Boolean)
+    list_date = Column(String)
+
+    fk_user = relationship('User')
     
-    	constraint foreign key fk_user(userno) references User(userno)
-);
 
-create table Comment (
-	cmt_id int unsigned not null auto_increment primary key,
-    	userno int unsigned not null,
-    	cmt_txt varchar(3000),
-    	cmt_date timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    	list_id int unsigned not null, 
-    	cmt_like int(11) not null default 0,
-    	cmt_hate int(11) not null default 0,
+class Comment(Base):
+    _tablename_ = 'Comment'
+    cmt_id = Column(Integer, primary_key=True)
+    userno = Column(Integer)
+    cmt_txt = Column(String)
+    cmt_date = Column(String)
+    list_id = Column(Integer)
+    cmt_like = Column(Integer)
+    cmt_hate =  Column(Integer)
+
+    fk_user = relationship('User')
+    fk_list = relationship('List')
     
-    	constraint foreign key fk_user(userno) references User(userno),
-    	constraint foreign key fk_list(list_id) references List(list_id)
-);
 
-create table Ranking (
-	list_id int unsigned not null,
-    	ranking_date timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    	rank int not null,
-    
-    	constraint foreign key fk_list(list_id) references List(list_id)
-);
 
-create table Likecnt (
-	list_id int unsigned not null,
-    	today_like int(11) not null default 0,
-    
-    	constraint foreign key fk_list(list_id) references List(list_id)
-);
+class Ranking(Base):
+    _tablename_ = 'Ranking'
+    list_id  = Column(Integer)
+    ranking_date = Column(String)
+    rank  = Column(Integer)
 
-create table DM (
-	userno int unsigned not null,
-    	receiver int unsigned not null,
-    	dm_txt varchar(4096),
-    	dm_date timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    
-    	constraint foreign key fk_user(userno) references User(userno)
-);
+    fk_list =  relationship('List')
 
+
+class Likecnt(Base):
+    _tablename_ = 'Likecnt'
+    list_id = Column(Integer)
+    today_like = Column(Integer)
+
+    fk_list = relationship('List')
+
+
+class DM(Base):
+    _tablename_ = 'DM'
+    userno = Column(Integer)
+    receiver = Column(Integer)
+    dm_txt = Column(String)
+    dm_date = Column(String)
+
+    fk_user = relationship('User')
