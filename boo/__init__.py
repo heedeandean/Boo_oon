46a0d1d05d0001ea9426e8d91603697c5dde9a01
eeuoo <lee512joo@gmail.com>
2019-03-27 12:56:40 +0900

    getzero

--- boo/__init__.py
+++ boo/__init__.py
@@ -23,21 +23,26 @@ def regist():
 
 @app.route('/boo', methods=['POST'])
 def regist_post():
-    username = request.values.get('username')
-    pw = request.values.get('pw')
-    pw2 = request.values.get('pw2')
-    birthdate = request.values.get('birthdate')
-    city = request.values.get('city')
-    gender = request.values.get('gender')
-    job = request.values.get('job')
-    email = request.values.get('email')
+    username = request.form.get('username')
+    pw = request.form.get('pw')
+    pw2 = request.form.get('pw2')
+    birthyear = request.form.get('birthyear')
+    birthmonth = request.form.get('birthmonth')
+    birthday = request.form.get('birthday')
+    birthdate = birthyear + getzero(birthmonth) + getzero(birthday)
+    sidogun = request.form.get('sidogun')
+    city = request.form.get('city')
+    addr = sidogun + ' ' + city
+    gender = request.form.get('gender')
+    job = request.form.get('job')
+    email = request.form.get('email')
     # joindt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     if pw != pw2:
         flash("암호를 정확히 입력하세요!!")
         return render_template("ecom_main.html", email=email, username=username)
     else:
-        u = Users(username, pw, birthdate, city, gender, job, email)
+        u = Users(username, pw, birthdate, addr, gender, job, email)
         try:
             db_session.add(u)
             db_session.commit()
@@ -59,3 +64,12 @@ def regist_post():
 def teardown_context(exception):
     print(">>> teardown context!!", exception)
     db_session.remove() 
+
+
+
+
+def getzero(md):
+    if len(md) == 2 :
+        return md
+    else :
+        return '0' + md
