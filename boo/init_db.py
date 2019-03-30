a776414f96a60d705920b3975a1eb9fba62597f8
heedeandean <7942ahj@hanmail.net>
2019-03-30 23:28:49 +0900

    회원가입 완료git add .

--- boo/init_db.py
+++ boo/init_db.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # connection 선언.
-mysql_url = 'mysql+pymysql://root:11@35.243.122.63:3306/boodb?charset=utf8' # 프로젝트 DB
+# mysql_url = 'mysql+pymysql://root:11@35.243.122.63:3306/boodb?charset=utf8' # 프로젝트 DB
 
-# mysql_url = 'mysql+pymysql://root:1234@localhost/project?charset=utf8' # 희진 집 DB 
+mysql_url = 'mysql+pymysql://root:1234@localhost/project?charset=utf8' # 희진 집 DB 
 
 engine = create_engine(mysql_url, echo=True, convert_unicode=True)
 
