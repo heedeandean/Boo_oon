3db86a68b5b6d3a8fd6321e9200aaa56cdf5df60
heedeandean <7942ahj@hanmail.net>
2019-04-13 12:52:26 +0900

    .

--- mys/set_query.sql
+++ mys/set_query.sql
@@ -1,6 +1,17 @@
-﻿SELECT * FROM boodb.Users;
+SELECT * FROM boodb.Users;
 
 DELETE FROM `boodb`.`Users`;
 
 # 서울 시간 설정.  
 set time_zone = 'Asia/Seoul';
+
+
+--  댓글을 달면 해당 cmt_count 이 1 증가하는 트리거.
+
+DELIMITER $$
+
+CREATE TRIGGER  tr_cmt_lists AFTER INSERT ON Cmt FOR EACH ROW
+BEGIN
+		update Lists set cmt_count  = cmt_count + 1 where Cmt.list_id = Lists.list_id;
+END$$
+DELIMITER ;
